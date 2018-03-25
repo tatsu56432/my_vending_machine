7c6664a2dd4f460fec6af0e4bc3adf8d02d553a3
t_nakajima <t_nakajima@bbmedia.co.jp>
2018-03-26 04:14:15 +0900

    functions.php

---
+++ src/htdocs/system/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+
+function escape($vars){
+    if(is_array($vars)){
+        return array_map("escape",$vars);
+    }else{
+        return htmlspecialchars($vars ,ENT_QUOTES,'UTF-8');
+    }
+
+}
+
+function validateInput($input = NULL){
+
+    if(!$input){
+        $input = $_POST;
+    }
+
+    $zip_code = isset($input['zip_code']) ? $input['zip_code'] : null;
+    $address = isset($input['address']) ? $input['address'] : null;
+    $name = isset($input['name']) ? $input['name'] : null;
+    $email = isset($input['email']) ? $input['email'] : null;
+
+
+
+
+
+}
